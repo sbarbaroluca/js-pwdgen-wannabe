b9b78d46841d079d77d2742291cdbe35216392fc
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-04-26 14:37:35 +0200

    + colore

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ var nomeUtente = prompt('Qual è il tuo nome?');
 
 // chiedo all'utente il suo cognome
 var cognomeUtente = prompt('Qual è il tuo cognome?');
+
+// chiedo all'utente il suo colore preferito
+var colorePreferito = prompt('Qual è il tuo colore preferito?');
+
+var password = nomeUtente + cognomeUtente + colorePreferito + "21";
